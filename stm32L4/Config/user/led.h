3af94dc5f99cbf4c7c2daccd3cd39ec58e003d91
JFM <cjdx20140910@126.com>
2019-01-06 16:12:47 +0800

    06/01/2019 生成了LED Config.

---
+++ stm32L4/Config/user/led.h
@@ -0,0 +1,30 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __led_H
+#define __led_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32l4xx_hal.h"
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* USER CODE BEGIN Private defines */
+#define RledSwitch(a)     HAL_GPIO_WritePin(GPIOE, GPIO_PIN_7, a)	
+#define GledSwitch(a)     HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, a)	
+#define BledSwitch(a)     HAL_GPIO_WritePin(GPIOE, GPIO_PIN_9, a)	
+/* USER CODE END Private defines */
+
+
+
+/* USER CODE BEGIN Prototypes */
+
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__ LED_H */
